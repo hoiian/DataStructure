8754d010b7b0aefc4ec9271a07cfd62eaf8cfc88
Wong Hoi Ian <hoiian_w@Wongde-MacBook-Pro.local>
2017-10-20 01:38:43 +0800

    ~~

--- hw2/hw2.c
+++ hw2/hw2.c
@@ -4,24 +4,25 @@ void hanoi(int,char,char,char);
 int i=0;
 int main(void){
     int n;
+    printf("Number of disks:");
     scanf("%d",&n);
     if(n<=64 && n>=3){
         hanoi(n,'A','B','C');
-        printf("%d..",i);
+        printf("Total steps:%d\n",i);
     }else{
-        printf("disks number should between 3 and 64.");
+        printf("Disks number should between 3 and 64.");
     }
     return 0;
 }
 
 void hanoi(int n , char a , char b, char c){
     if(n==1){
-        printf("disk%d move from %c to %c.\n",n,a,c);
+        printf("Disk%d moves from %c to %c.\n",n,a,c);
         i++;
         return;
     }else{
         hanoi(n-1, a, c, b);
-        printf("disk%d move from %c to %c.\n",n,a,c);
+        printf("Disk%d moves from %c to %c.\n",n,a,c);
         i++;
         hanoi(n-1,b,a,c);
     }
